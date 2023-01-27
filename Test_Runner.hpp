43ee43e1b21e796ac29f014aa87bf70fadb6dac1
Dmitry Morozov <9984398@gmail.com>
2023-01-27 22:22:14 +0300

    Test Framework : class TestRunner : Initial commit

---
+++ Test_Runner.hpp
@@ -0,0 +1,12 @@
+//
+// Created by Dmitry Morozov on 27/1/23.
+//
+
+#ifndef UTEST_MAIN__TEST_RUNNER_HPP_
+#define UTEST_MAIN__TEST_RUNNER_HPP_
+
+class TestRunner {
+
+};
+
+#endif //UTEST_MAIN__TEST_RUNNER_HPP_
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <map>
+#include <set>
 
 template<class T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &s) {
@@ -11,6 +13,18 @@ std::ostream &operator<<(std::ostream &os, const std::vector<T> &s) {
   }
 
   return os << "]";
+}
+
+//Override the << operator for map
+template<class K, class V>
+std::ostream &operator<<(std::ostream &os, const std::map<K, V> map_object) {
+  os << "{" << std::endl;
+
+  for (const auto &[key, value] : map_object) {
+    std::cout << key << " : " << value << std::endl;
+  }
+
+  return os << "}";
 }
 
 template<class T, class U>
@@ -57,28 +71,80 @@ void Test_div() {
   AssertEqual(Div(8.999, 3.0), 3, " Line 57");
 }
 
+void Test_Dict() {
+  std::map<int, std::string> dict1 = {
+
+      {9984444, "Dmitry"},
+      {9372837, "Irina"},
+      {6000000, "Taxi"}
+
+  };
+
+  AssertEqual(dict1.size(), 3, "Size of Dict is 3 ");
+
+  dict1[1000000] = "Boutique";
+
+  AssertEqual(dict1.size(), 4, "Size of Dic is now 4");
+
+  AssertEqual(dict1.at(9984444), "Dmitry", "9984444 is Dmitry");
+
+  AssertEqual(dict1[6000000], "Vlad", "6000000 is a Taxi");
+
+}
+
+void Test_Dict2() {
+  std::map<int, std::string> m1 = {
+      {9984444, "Dmitry"},
+      {9372837, "Irina"},
+      {6000000, "Taxi"}
+  };
+  std::map<int, std::string> m2 = {
+      {9372837, "Irina"},
+      {6000000, "Taxi"},
+      {9984444, "Dmitry"}
+  };
+
+  AssertEqual(m1, m2, " Expect they are equal");
+
+}
+
+//TODO: Implement set types for AssertEqual
+void Test_Set() {
+  std::set<int> s1 = {1, 4, 6, 8};
+  std::set<int> s2 = {10, 2, 4, 5, 7};
+
+//  AssertEqual(s1.size(), 4, "The size of set is 4");
+//  AssertEqual(s1, s2, "This set are not Equal");
+
+}
+
 int main() {
 
-  std::vector<int> a = {1, 2, 3, 4, 5};
-  std::vector<int> b = {1, 2, 3, 4, 5};
-  std::vector<int> c = {1, 2};
-
-  Assert(Add(2, 5) == 7, "Test 2 + 5");
-  std::cout << " 2 + 5  = 7 Passed!" << std::endl;
-
+//  std::vector<int> a = {1, 2, 3, 4, 5};
+//  std::vector<int> b = {1, 2, 3, 4, 5};
+//  std::vector<int> c = {1, 2};
+//
+//  Assert(Add(2, 5) == 7, "Test 2 + 5");
+//  std::cout << " 2 + 5  = 7 Passed!" << std::endl;
+//
 
 
   //Test_Add();
 //  Test_div();
-  AssertEqual(Concat("Hello ", "world"), "Hello world", "Line 73 ");
+//  AssertEqual(Concat("Hello ", "world"), "Hello world", "Line 73 ");
+//
+//  AssertEqual(a, b, "Compare vectors a & b ");
+//  std::cout << "a = b Passed " << std::endl;
+//  AssertEqual(a, c, "Line 77");
+//  std::cout << "a = c Passed " << std::endl;
+//
+//  AssertEqual(a, std::vector({1, 2, 3, 4, 5}), "Line 80");
+//  std::cout << "a = {1,2,3,4,5} Passed " << std::endl;
 
-  AssertEqual(a, b, "Compare vectors a & b ");
-  std::cout << "a = b Passed " << std::endl;
-  AssertEqual(a, c, "Line 77");
-  std::cout << "a = c Passed " << std::endl;
+//  Test_Dict();
 
-  AssertEqual(a, std::vector({1, 2, 3, 4, 5}), "Line 80");
-  std::cout << "a = {1,2,3,4,5} Passed " << std::endl;
+  Test_Dict2();
+  std::cout << "Dict Test Passed" << std::endl;
 
   return 0;
 
